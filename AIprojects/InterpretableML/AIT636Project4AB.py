65756a04903ab24784989c1785d85bcc1dfda879
MyCadence <arronbirhanu03@gmail.com>
2026-02-14 04:59:22 -0500

    Updated K-Clustering Project: True Clusters show alongside each K Test

--- AIprojects/InterpretableML/AIT636Project4AB.py
+++ AIprojects/InterpretableML/AIT636Project4AB.py
@@ -7,31 +7,36 @@ data = make_blobs(n_samples=200, n_features=2, centers=4, cluster_std=1.6, rando
 
 points = data[0]
 labels = data[1]
-
 feature_1 = points[:, 0]
 feature_2 = points[:, 1]
-plt.subplot(3, 2, 1)
-plt.scatter(feature_1, feature_2, c=labels, s=10, cmap='viridis')
-plt.title('True Clusters')
-plt.xlim(-15, 15)
-plt.ylim(-15, 15)
-# plt.show()
+
+
 
 # import the library
 from sklearn.cluster import KMeans
 # Create the KMeans object and specify its characteristics
-for k in range(2, 7):
+
+plt.figure(figsize=(5, 25))#Resize sub plots for Sample Report
+for i,k in enumerate(range(2, 7)):
+
+    #Left Column (True Clusters)
+    plt.subplot(5, 2, 2*i + 1)
+    plt.scatter(feature_1, feature_2, c=labels, s=10, cmap='viridis')
+    plt.title('True Clusters')
+    plt.xlim(-15, 15)
+    plt.ylim(-15, 15)
+
+    #Right Column (Predicted)
     kmeans_method = KMeans(n_clusters=k)
     kmeans_method.fit(points)
-
-    # Predict the cluster for a set of points
     labels_predicted = kmeans_method.predict(points)
 
-    plt.subplot(3, 2, k)
+    plt.subplot(5, 2, 2*i + 2)
     plt.scatter(feature_1, feature_2, c=labels_predicted, s=10, cmap='viridis')
     plt.title(f'Predicted Clusters: K={k}')
     plt.xlim(-15, 15)
     plt.ylim(-15, 15)
 
-plt.subplots_adjust(hspace=0.5, wspace=0.3)
+plt.subplots_adjust(hspace=0.6, wspace=0.2)
 plt.show()
+
